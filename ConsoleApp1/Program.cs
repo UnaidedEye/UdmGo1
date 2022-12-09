a45ed6c893839004a1a0dfd8b3a4cb8311cd01b0
UnaidedEye <dulesovandrei@gmail.com>
2022-12-09 13:50:06 +0400

    Done

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 15 == 0) Console.Write("FizzBuzz ");
+    else if (i % 5 == 0) Console.Write("Buzz ");
+    else if (i % 3 == 0) Console.Write("Fizz ");
+    else Console.Write(i + " ");
+    if (i % 10 == 0) Console.WriteLine();
+}
